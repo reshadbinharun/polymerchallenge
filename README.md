bbf5acca767096678007eb1d91ea290f854dee30
reshadbinharun <md.bin_harun@tufts.edu>
2018-06-12 13:55:39 -0400

    first implementation

--- README.md
+++ README.md
@@ -3,6 +3,23 @@ Developer: Md Reshad Bin Harun
 Date: 12th June, 2018
 
 Design:
+Food Show CRM:
+Client-side:
+* Table object asynchronously loaded every-time a sorting is done or update is invoked
+Server-sider:
+* SQL database to store in table all fields. On loading client-side HTML, the table is pulled from the database as a JSON object
+* Status updates are made using a different API route
 
 Implementation:
+1. Server-side:
+* install dependencies using node
+* set up route "GET" for getting database as JSON
+* set up route "POST" for making a status update
+* set up route "POST" to add a company
 
+2. Client-side:
+* HTML 1:
+What would you like to do?
+    1. Register company
+    2. Check Status
+    3. Update Status
--- index.js
+++ index.js
@@ -1,10 +1,55 @@
 const path = require('path');
+const { Client } = require('pg');
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended : true}));
 app.listen(process.env.PORT || 3000);
 
+//setting database connection
+DATABASE_URL = config.env.db;
+const client = new Client({
+connectionString: DATABASE_URL,
+ssl: true,
+});
+client.connect();
+
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+//return all companies in database as JSON object to be parsed on client-side
+app.get('/status', function (req, res) {
+	var query_string = 'SELECT * FROM CRM';
+	console.log(query_string);
+	client.query(query_string, (err, res2) => {
+	 	if (err) throw err;
+	  	res.send(res2);
+	  	});
+});
+
+//API to update the status of a particular company
+app.post('/update', function (req, res) {
+	var status = req.body.status;
+	var company = req.body.company;
+	var query = 'UPDATE CRM SET status=\''+status+'\' WHERE company=\''+company+'\';';
+	client.query(query_string, (err, res2) => {
+	 	if (err) throw err;
+	  	res.send("Status update successful!");
+	});
+});
+
+//API to register a company
+app.post('/register', function (req, res) {
+	var status = req.body.status;
+	var company = req.body.company;
+	var email = req.body.email;
+	var name = req.body.name;
+	var query = 'insert into CRM (name, company, email, status) values ('+ name +', '+company+', '+email+', '+status+');'
+	client.query(query_string, (err, res2) => {
+	 	if (err) throw err;
+	  	res.send("Registration Successful!");
+	});
 });
